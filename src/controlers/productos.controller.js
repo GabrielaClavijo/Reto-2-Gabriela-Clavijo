2854d334e599267a5e751bb89274fc5d3bb170f9
GabrielaClavijo <gabriela.clavijo@omni.pro>
2022-09-04 23:33:36 -0500

    Se realiza actualizar producto

--- src/controlers/productos.controller.js
+++ src/controlers/productos.controller.js
@@ -37,9 +37,7 @@ export const postProducto =(req, resp) => {
     if (!sku   || !nombre   || !precio   || !url   || !marca   || !descripcion   || !iva   || !descuento   || !fecha_creacion  ) {
       return resp.status(400).json({ message: "registro incompleto" })
     }
-    console.log(sku);
     const nuevoProducto = { sku, nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion }
-    console.log(products);
     products.push(nuevoProducto)
     fs.writeFileSync('./helpers/productos.json', JSON.stringify(products))
     resp.send(200, { message: "registro completo" })
@@ -48,3 +46,50 @@ export const postProducto =(req, resp) => {
     resp.send(error.message)
   }
 }
+
+//Actualizar producto
+
+export const putProducto =(req, resp) => {
+  try {
+    const skuupt = req.params.sku
+    const producto=products.find(producto=>producto.sku ===skuupt)
+    let{ nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion } = req.body
+    if (!nombre) {
+      nombre =producto.nombre
+    }
+    if (!precio) {
+      precio = producto.precio
+    }
+    if (!url) {
+      url = producto.url
+    }
+    if (!marca) {
+      marca = producto.marca
+    }
+    if (!descripcion) {
+      descripcion = producto.descripcion
+    }
+    if (!iva) {
+      iva = producto.iva
+    }
+    if (!descuento) {
+      descuento = producto.descuento
+    }
+    if (!inventario) {
+      inventario = producto.inventario
+    }
+    if (!fecha_creacion) {  
+      fecha_creacion = producto.fecha_creacion
+    }
+    
+    const nuevoProducto = { sku: skuupt, nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion }
+    
+    let posicion = products.indexOf(producto)
+    products.splice(posicion, 1, nuevoProducto)
+    fs.writeFileSync('./helpers/productos.json', JSON.stringify(products))
+    resp.send(200, { message: "registro completo" })
+  } catch (error) {
+    resp.status(500)
+    resp.send(error.message)
+    }
+  }
--- src/rutas/productos.routes.js
+++ src/rutas/productos.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getProducto, getProductos, postProducto } from "../controlers/productos.controller.js";
+import { getProducto, getProductos, postProducto, putProducto } from "../controlers/productos.controller.js";
 
 const router=Router()
 router.get('/admin/producto', getProductos) 
 router.get('/admin/producto/:sku', getProducto) 
 router.post('/admin/producto', postProducto)
+router.put('/admin/producto/:sku', putProducto)
 
 export default router;
