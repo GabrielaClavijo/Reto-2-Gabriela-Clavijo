e63cb4e6ae7e5740a97ae1bab6ce0980cbeea410
GabrielaClavijo <gabriela.clavijo@omni.pro>
2022-09-05 22:00:45 -0500

    Se realiza consultar producto app movil

--- src/controlers/productos.controller.js
+++ src/controlers/productos.controller.js
@@ -1,130 +1,199 @@
 import fs from "fs";
-const jsonproducts = fs.readFileSync("./helpers/productos.json")
-const products = JSON.parse(jsonproducts)
+const jsonproducts = fs.readFileSync("./helpers/productos.json");
+const products = JSON.parse(jsonproducts);
 
 //Listar productos
 export const getProductos = (req, resp) => {
-    try {
-        
-        resp.json(products)
-    } catch (error)
-     {
-        resp.status(500)
-        resp.send(error.message)
-    }
-}
+  try {
+    resp.json(products);
+  } catch (error) {
+    resp.status(500);
+    resp.send(error.message);
+  }
+};
 
 //Consultar producto
 
 export const getProducto = (req, resp) => {
   try {
-    const sku = req.params.sku
-    const producto=products.find(producto=>producto.sku ===sku)
-    resp.json(producto)
-    } catch (error)
-     {
-      resp.status(500)
-      resp.send(error.message)
-    }
-
-}
+    const sku = req.params.sku;
+    const producto = products.find((producto) => producto.sku === sku);
+    resp.json(producto);
+  } catch (error) {
+    resp.status(500);
+    resp.send(error.message);
+  }
+};
 
 //Insertar producto
 
-export const postProducto =(req, resp) => {
+export const postProducto = (req, resp) => {
   try {
-    const { sku, nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion } = req.body
-    if (!sku   || !nombre   || !precio   || !url   || !marca   || !descripcion   || !iva   || !descuento   || !fecha_creacion  ) {
-      return resp.status(400).json({ message: "registro incompleto" })
+    const {
+      sku,
+      nombre,
+      precio,
+      url,
+      marca,
+      descripcion,
+      iva,
+      descuento,
+      inventario,
+      fecha_creacion,
+    } = req.body;
+    if (
+      !sku ||
+      !nombre ||
+      !precio ||
+      !url ||
+      !marca ||
+      !descripcion ||
+      !iva ||
+      !descuento ||
+      !fecha_creacion
+    ) {
+      return resp.status(400).json({ message: "registro incompleto" });
     }
-    const nuevoProducto = { sku, nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion }
-    products.push(nuevoProducto)
-    fs.writeFileSync('./helpers/productos.json', JSON.stringify(products))
-    resp.send(200, { message: "registro completo" })
+    const nuevoProducto = {
+      sku,
+      nombre,
+      precio,
+      url,
+      marca,
+      descripcion,
+      iva,
+      descuento,
+      inventario,
+      fecha_creacion,
+    };
+    products.push(nuevoProducto);
+    fs.writeFileSync("./helpers/productos.json", JSON.stringify(products));
+    resp.send(200, { message: "registro completo" });
   } catch (error) {
-    resp.status(500)
-    resp.send(error.message)
+    resp.status(500);
+    resp.send(error.message);
   }
-}
+};
 
 //Actualizar producto
 
-export const putProducto =(req, resp) => {
+export const putProducto = (req, resp) => {
   try {
-    const skuupt = req.params.sku
-    const producto=products.find(producto=>producto.sku ===skuupt)
-    let{ nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion } = req.body
+    const skuupt = req.params.sku;
+    const producto = products.find((producto) => producto.sku === skuupt);
+    let {
+      nombre,
+      precio,
+      url,
+      marca,
+      descripcion,
+      iva,
+      descuento,
+      inventario,
+      fecha_creacion,
+    } = req.body;
     if (!nombre) {
-      nombre =producto.nombre
+      nombre = producto.nombre;
     }
     if (!precio) {
-      precio = producto.precio
+      precio = producto.precio;
     }
     if (!url) {
-      url = producto.url
+      url = producto.url;
     }
     if (!marca) {
-      marca = producto.marca
+      marca = producto.marca;
     }
     if (!descripcion) {
-      descripcion = producto.descripcion
+      descripcion = producto.descripcion;
     }
     if (!iva) {
-      iva = producto.iva
+      iva = producto.iva;
     }
     if (!descuento) {
-      descuento = producto.descuento
+      descuento = producto.descuento;
     }
     if (!inventario) {
-      inventario = producto.inventario
+      inventario = producto.inventario;
     }
-    if (!fecha_creacion) {  
-      fecha_creacion = producto.fecha_creacion
+    if (!fecha_creacion) {
+      fecha_creacion = producto.fecha_creacion;
     }
-    
-    const nuevoProducto = { sku: skuupt, nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion }
-    
-    let posicion = products.indexOf(producto)
-    products.splice(posicion, 1, nuevoProducto)
-    fs.writeFileSync('./helpers/productos.json', JSON.stringify(products))
-    resp.send(200, { message: "registro completo" })
+
+    const nuevoProducto = {
+      sku: skuupt,
+      nombre,
+      precio,
+      url,
+      marca,
+      descripcion,
+      iva,
+      descuento,
+      inventario,
+      fecha_creacion,
+    };
+
+    let posicion = products.indexOf(producto);
+    products.splice(posicion, 1, nuevoProducto);
+    fs.writeFileSync("./helpers/productos.json", JSON.stringify(products));
+    resp.send(200, { message: "registro completo" });
   } catch (error) {
-    resp.status(500)
-    resp.send(error.message)
-    }
-}
-  
+    resp.status(500);
+    resp.send(error.message);
+  }
+};
 
 //Eliminar producto
 export const deleteProducto = (req, resp) => {
   try {
-    const skuupt = req.params.sku
-    const producto = products.find(producto => producto.sku === skuupt)
-    let posicion = products.indexOf(producto)
-    products.splice(posicion, 1)
-    resp.send(200, { message: "registro eliminado" })
-    fs.writeFileSync('./helpers/productos.json', JSON.stringify(products))
+    const skuupt = req.params.sku;
+    const producto = products.find((producto) => producto.sku === skuupt);
+    let posicion = products.indexOf(producto);
+    products.splice(posicion, 1);
+    resp.send(200, { message: "registro eliminado" });
+    fs.writeFileSync("./helpers/productos.json", JSON.stringify(products));
   } catch (error) {
-    resp.status(500)
-    resp.send(error.message)
+    resp.status(500);
+    resp.send(error.message);
   }
-}
+};
 
 //Listar productos App móvil
-export const getProductosApp=(req, resp) => {
-    try {
-      const productoExiste=products.filter(({inventario}) => inventario>0)
-      const infoProducto = productoExiste.map(({sku,nombre,url, marca, precio, iva, descuento}) => {
-        const precioFinal = precio - (precio * descuento) + (precio * iva)
+export const getProductosApp = (req, resp) => {
+  try {
+    const productoExiste = products.filter(({ inventario }) => inventario > 0);
+    const infoProducto = productoExiste.map(
+      ({ sku, nombre, url, marca, precio, iva, descuento }) => {
+        const precio_final = precio - precio * descuento + precio * iva;
         return {
-          sku, nombre, url, marca, precioFinal
-        }
-      })
-      console.log(infoProducto);
-      resp.json(infoProducto)
-    } catch (error)
-     {
-        resp.status(500)
-        resp.send(error.message)
-    }
-}
+          sku,
+          nombre,
+          url,
+          marca,
+          precio_final,
+        };
+      }
+    );
+    console.log(infoProducto);
+    resp.json(infoProducto);
+  } catch (error) {
+    resp.status(500);
+    resp.send(error.message);
+  }
+};
+
+//Consultar producto app móvil
+export const getConsultarApp = (req, resp) => {
+  try {
+    const skuApp = req.params.sku;
+    const producto = products.find(({ sku }) => sku === skuApp);
+
+    const { sku, nombre, url, marca, precio, iva, descuento, descripcion } =
+      producto;
+    const precio_final = precio - precio * descuento + precio * iva;
+    resp.json({ sku, nombre, url, marca, precio, iva, descuento, descripcion, precio_final });
+  } catch (error) {
+    resp.status(500);
+    resp.send(error.message);
+  }
+};
--- src/rutas/productos.routes.js
+++ src/rutas/productos.routes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
-import { deleteProducto, getProducto, getProductos, getProductosApp, postProducto, putProducto } from "../controlers/productos.controller.js";
+import {
+  deleteProducto,
+  getConsultarApp,
+  getProducto,
+  getProductos,
+  getProductosApp,
+  postProducto,
+  putProducto,
+} from "../controlers/productos.controller.js";
 
 //Rutas que se llaman en cada método de productos.controller.js
-const router=Router()
-router.get('/admin/producto', getProductos) 
-router.get('/admin/producto/:sku', getProducto) 
-router.post('/admin/producto', postProducto)
-router.put('/admin/producto/:sku', putProducto)
-router.delete('/admin/producto/:sku', deleteProducto)
-router.get('/producto', getProductosApp)
+const router = Router();
+router.get("/admin/producto", getProductos);
+router.get("/admin/producto/:sku", getProducto);
+router.post("/admin/producto", postProducto);
+router.put("/admin/producto/:sku", putProducto);
+router.delete("/admin/producto/:sku", deleteProducto);
+router.get("/producto", getProductosApp);
+router.get("/producto/:sku", getConsultarApp);
 
 export default router;
