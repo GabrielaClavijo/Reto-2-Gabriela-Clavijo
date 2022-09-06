1be563b167b4059c759aaae51b0152cbd8d4d0f2
GabrielaClavijo <gabriela.clavijo@omni.pro>
2022-09-05 22:02:42 -0500

    Se elimina log

--- src/controlers/ordenes.controller.js
+++ src/controlers/ordenes.controller.js
@@ -1,34 +1,30 @@
 import fs from "fs";
-const jsonorders = fs.readFileSync("./helpers/ordenes.json")
-const orders = JSON.parse(jsonorders)
-const jsonproducts = fs.readFileSync("./helpers/productos.json")
-const products = JSON.parse(jsonproducts)
+const jsonorders = fs.readFileSync("./helpers/ordenes.json");
+const orders = JSON.parse(jsonorders);
+const jsonproducts = fs.readFileSync("./helpers/productos.json");
+const products = JSON.parse(jsonproducts);
 
 //Consultar ordenes de compra
 export const getOrdenes = (req, resp) => {
   try {
-    const ordenesp = orders.map(orden => {
-      const list = []
-      orden.productos.forEach(element => {
-        console.log(element);
-        const producto=products.find(e=>e.sku===element)
+    const ordenesp = orders.map((orden) => {
+      const list = [];
+      orden.productos.forEach((element) => {
+        const producto = products.find((e) => e.sku === element);
         if (producto) {
-          const { sku, nombre, marca, url } =producto
-          list.push({sku, nombre, marca, url})
-          
-        }
-        else { 
-          list.push(element)
+          const { sku, nombre, marca, url } = producto;
+          list.push({ sku, nombre, marca, url });
+        } else {
+          list.push(element);
         }
       });
-      orden.productos = list
-      return orden
-    })
+      orden.productos = list;
+      return orden;
+    });
     console.log(ordenesp);
-    resp.json(ordenesp)
-
+    resp.json(ordenesp);
   } catch (error) {
-    resp.status(500)
-    resp.send(error.message)
+    resp.status(500);
+    resp.send(error.message);
   }
-}
+};
